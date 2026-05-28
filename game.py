0ea7dfb3b6e8f8cfa0aa18ddd1efd28e0c091f99
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-28 16:37:12 +0545

    playground setup almost complete

--- game.py
+++ game.py
@@ -1,7 +1,6 @@
 from settings import *
 from circle import Circle
 from wrestler import *
-from random import choices
 
 class Game(Circle):
 	def __init__(self, players: Wrestler):
@@ -12,53 +11,88 @@ class Game(Circle):
 		super().__init__(x, y, radius, BLUE)
 
 		self.players = players
-		self.player1 = None
-		self.player2 = None
+		# i => player1, j => player2
+		self.generation = 1
+		self.i = 0
+		self.j = 1
 		self.timer = 0
 
-	def match(self, player1: Wrestler, player2: Wrestler):
-		self.player1 = player1
-		self.player2 = player2
+		self.players[self.i].pos = self.pos - Vector(200, 0)
+		self.players[self.j].pos = self.pos + Vector(200, 0)
+
+	def next_match(self):
+		# setting up players
+		if self.i + 2 >= len(self.players) and self.j + 1 >= len(self.players):
+			self.next_gen()
+		elif self.j + 1 >= len(self.players):
+			self.i += 1
+			self.j = self.i + 1
+		else:
+			self.j += 1
+
+		self.players[self.i].pos = self.pos - Vector(200, 0)
+		self.players[self.j].pos = self.pos + Vector(200, 0)
+
+			
+
 
 	def update(self):
-		if not (self.player1 and self.player2):
-			return
+		[i,j] = [self.i, self.j]
+		if not (self.players[i] and self.players[j]):
+			print(len(self.players), i, j)
+			raise "Player indices not available"
 		
-		self.player1.update()
-		self.player1.update()
+		self.players[i].update()
+		self.players[i].opponent = self.players[j]
+		self.players[j].update()
+		self.players[j].opponent = self.players[i]
 		if self.check_gameover():
-			pass
+			self.next_match()
 
-		if check_collision(self.player1, self.player2):
-			resolve_penetration(self.player1, self.player2)
-			collision_resolution(self.player1, self.player2)
+		if check_collision(self.players[i], self.players[j]):
+			resolve_penetration(self.players[i], self.players[j])
+			collision_resolution(self.players[i], self.players[j])
 		
 		self.timer += 1
 
 	def check_gameover(self):
-		player1_out = (self.pos - self.player1.pos).mag() > self.radius-self.border - self.player1.radius
-		player2_out = (self.pos - self.player2.pos).mag() > self.radius-self.border - self.player2.radius
-
-		if player1_out or player2_out or self.timer % (60*5):
+		[i,j] = [self.i, self.j]
+		player1_out = (self.pos - self.players[i].pos).mag() > self.radius-self.border - self.players[i].radius
+		player2_out = (self.pos - self.players[j].pos).mag() > self.radius-self.border - self.players[j].radius
+		if player1_out or player2_out or self.timer % (60*10)==0:
 			if player2_out and not player1_out:
-				self.player1.score += 10
-				self.player2.score -= 10
+				self.players[i].score += 10
+				self.players[j].score -= 10
 			if player1_out and not player2_out:
-				self.player1.score -= 10
-				self.player2.score += 10
+				self.players[i].score -= 10
+				self.players[j].score += 10
 
 			if self.timer % (60*5):
-				self.player1.score -= 5
-				self.player2.score -= 5
+				self.players[i].score -= 5
+				self.players[j].score -= 5
 
+			self.timer = 0
+			return True
+		else:
+			return False
 
-				# setting up next match
-				self.timer = 0
-				return True
-			else:
-				return False
+	def next_gen(self):
+		topk: list[AgenticWrestler] = sorted(self.players, key=lambda player: player.score, reverse=True)[:2]
+		self.players = []
+		for player in topk:
+			self.players += player.mutate(2, diversity=.01)
+		self.players += topk
+		self.generation += 1
+		self.i = 0
+		self.j = 1
+
 
 	def draw(self):
 		super().draw()
-		self.player1.draw()
-		self.player2.draw()
+		self.players[self.i].draw()
+		self.players[self.j].draw()
+
+	def draw_status(self):
+		draw_text(f"Generation: {self.generation}", 20, 20, 20, WHITE)
+		draw_text(f"Player Count: {len(self.players)}", 20, 40, 20, WHITE)
+		draw_text(f"Player {self.i} vs {self.j}", 20, 60, 20, WHITE)
--- main.py
+++ main.py
@@ -3,9 +3,9 @@ from wrestler import *
 from game import Game
 
 init_window(WINDOW_WIDTH, WINDOW_HEIGHT, "Sumo Physics AI")
-set_target_fps(60)
+set_target_fps(360)
 
-players = [AgenticWrestler() for _ in range(100)]
+players = [AgenticWrestler() for _ in range(2)]
 
 game = Game(players)
 
@@ -17,6 +17,7 @@ while not window_should_close():
 	begin_drawing()
 	clear_background((55, 55, 55))
 	game.draw()
+	game.draw_status()
 	end_drawing()
 
 close_window()
