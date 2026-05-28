0348fe589413102703c98b45d5058d123e447e6f
Pramendra Sharma <sharmarahul111.psr@gmail.com>
2026-05-28 11:18:00 +0545

    using vector

--- circle.py
+++ circle.py
@@ -1,11 +1,11 @@
 from settings import *
+from vector import Vector
 class Circle:
 	def __init__(self, x, y, radius, color):
-		self.x = x
-		self.y = y
+		self.pos = Vector(x, y)
 		self.radius = radius
 		self.color = color
 
 	def draw(self):
-		draw_circle(int(self.x), int(self.y), self.radius*1.05, BLACK)
-		draw_circle(int(self.x), int(self.y), self.radius, self.color)
+		draw_circle(int(self.pos.x), int(self.pos.y), self.radius*1.05, BLACK)
+		draw_circle(int(self.pos.x), int(self.pos.y), self.radius, self.color)
--- wrestler.py
+++ wrestler.py
@@ -1,6 +1,9 @@
 from settings import *
 from circle import Circle
+from vector import Vector
 
 class Wrestler(Circle):
 	def __init__(self, x, y, radius):
 		super().__init__(x, y, radius, YELLOW)
+		self.vel = Vector(0, 0)
+		self.acc = Vector(0, 0)
